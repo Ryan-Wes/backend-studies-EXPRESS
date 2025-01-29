fcde145c7a5c719166e2d387891318707f875531
WRyan <wesley.ryan03@gmail.com>
2025-01-29 14:30:10 -0300

    Add files via upload

--- rotas-com-express.js
+++ rotas-com-express.js
@@ -2,17 +2,18 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
- res.send('Home page');
+ res.send('Olá, mundo!');
 });
 
 app.get('/about', (req, res) => {
- res.send('About page');
+ res.send('Esta é a página Sobre nós!');
 });
 
-app.use((req, res) => {
- res.status(404).send('Page not found');
+app.get('/usuarios/:nome', (req, res) => {
+ const nome = req.params.id;
+ res.send(`Este é o perfil do usuário ${nome}`);
 });
 
 app.listen(3000, () => {
- console.log('Servidor em execução em http://localhost:3000/');
+ console.log('Servidor iniciado na porta 3000.');
 });
